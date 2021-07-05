a96da5955398f4419bfe0027a83110db6334689e
Scott Coons <scoons@cnm.edu>
2021-07-05 13:24:19 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+window.onLoad = function () {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+
+    form.addEventListener("submit", (event) => {
+        handleItemForm();
+    })
+}
+
+function handleItemForm(event, formRef) {
+    if(event.preventDefault()) {
+        event.preventDefault();
+    }
+
+    console.log("Event", event);
+
+    let itemHtml = addItemToShoppingList ();
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+    return false;
+}
+
+function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount")
+
+}
+
+function createListItemHtml() {
+    return '<li>
+                Item Name - Amount
+            </li>
+    ';
+
+}
